3a9badcb466544b8c93ebb801d2c42325053a102
Isabella Oliveira Prudente <62033993+ioprudente@users.noreply.github.com>
2022-05-29 14:04:56 -0300

    Refatora.

--- src/main/java/br/com/oobj/easybill/repository/ClienteRepository.java
+++ src/main/java/br/com/oobj/easybill/repository/ClienteRepository.java
@@ -1,10 +1,10 @@
 package br.com.oobj.easybill.repository;
 
-import br.com.oobj.easybill.model.Produto;
+import br.com.oobj.easybill.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Produto, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
 
 }
